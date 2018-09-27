8e5bf61443e3895a2086772b9f7281e7c926bdee
michael <814043474@qq.com>
2018-09-28 00:09:56 +0800

    first commit

---
+++ 快速排序.js
@@ -0,0 +1,20 @@
+let quickSort = (arr) => {
+    if(arr.length <= 1) {
+        return arr;
+    }
+    const middleIndex = Math.floor(arr.length/2);
+    const middle = arr.splice(middleIndex, 1)[0];
+    const left = [];
+    const right = [];
+    for (let i=0 ; i < arr.length; i++) {
+        if(arr[i] < middle) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+    return quickSort(left).concat([middle], quickSort(right));
+    
+}
+
+console.log(quickSort([3,4,4343, 444, 55]));
